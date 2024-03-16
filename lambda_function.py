398148c39ab2c4f400c56b04fe4a3e74dfd4539d
vijaykalsi <vijay.kalsi@outlook.com>
2024-03-16 21:57:47 +0530

    update lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,31 @@
-import json
-import requests
+import boto3
 import pandas as pd
+from io import StringIO
 
 def lambda_handler(event, context):
-    
-    print("Event Data -> ", event)
-    response = requests.get("https://www.google.com/")
-    print(response.text)
+    # Get the S3 bucket and object key from the Lambda event trigger
+    print(event)
+    srcbucket = event['Records'][0]['s3']['bucket']['name']
+    srckey = event['Records'][0]['s3']['object']['key']
+    tgtbucket='doordash-target-zn-vj'
+    tgtkey='2024-03-09-processed_output.json'
 
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Demo Completed !!!')
+    # Use boto3 to get the CSV file from S3
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket=srcbucket, Key=srckey)
+    file_content = response["Body"].read().decode('utf-8')
+
+    # Read the content using pandas
+    data = pd.read_csv(StringIO(file_content))
+    print(data)
+    #Filter records where status is "delivered"
+    delivered_df = data[data['status'] == 'delivered']
+    print('Delivered Data :n', delivered_df)
+
+    # write filtered data frame to target S3
+    filetoupload=StringIO()
+    delivered_df.to_json(filetoupload)
+
+    response = s3_client.put_object(Body=filetoupload.getvalue(), Bucket=tgtbucket, Key=tgtkey, )
+    print(response)
+
