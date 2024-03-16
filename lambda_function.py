13794c5d6e6248effbe51c4ca9184412339f2321
vijaykalsi <vijay.kalsi@outlook.com>
2024-03-16 08:50:26 +0530

    Modified buildspec.yml

--- lambda_function.py
+++ lambda_function.py
@@ -3,11 +3,12 @@ import requests
 import pandas as pd
 
 def lambda_handler(event, context):
-    print("Event Data ->", event)
-    response = requests.get("https://ww.google.com/")
+    
+    print("Event Data -> ", event)
+    response = requests.get("https://www.google.com/")
     print(response.text)
 
-    d={'col1': [1,2], 'col2':[3,4]}
-    df=pd.DataFrame(data=d)
+    d = {'col1': [1,2], 'col2': [3,4]}
+    df = pd.DataFrame(data=d)
     print(df)
-    print('demo completed')
+    print('Demo Completed !!!')
